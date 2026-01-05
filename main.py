8bcac5752998fcec35beba861b7da2b575a5a8dc
LemonMan67 <liammcnally800@gmail.com>
2026-01-05 23:19:44 +0000

    cuh

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import json 
 from pathlib import Path
 
-with open ("battalion.json" , "r") as file:
-    f = json.load(file)
-for x in f['tank']:
-    print (x)
+def loadjson (filepath):
+    with open (filepath , "r" ) as f:
+        return json.load (f)
+
+def writejson (filepath , update) :
+    with open (filepath , "w") as f:
+        json.dump(f , update , indent=4 )
+
+class Battalion:
+    def __init__(self, filepath = "battalion.json"):
+        self.path = Path (filepath)  
+        self.info = loadjson (self.path)
+
+    def Bcheck (self ,stat):
+        for stat , details  in self.info ["tank"].tank : 
+            print (f" {type} : {details ['hp']} ") #ask for help with json and oop tmr gng 
 
 
-a = json.dumps(f , indent = 2)
-print(a)
-
+battalion = Battalion()
+battalion.Bcheck("hp")
+    
