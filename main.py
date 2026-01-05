58516a32b0383266573b5235d9ccbe2d3c741f0e
LemonMan67 <liammcnally800@gmail.com>
2026-01-05 20:36:02 +0000

    nuh

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import json
 from pathlib import Path
 
 with open ("battalion.json" , "r") as file:
-    json.load(file)
+    f = json.load(file)
+for x in f['tank']:
+    print (x)
+
+
+a = json.dumps(f , indent = 2)
+print(a)
 
