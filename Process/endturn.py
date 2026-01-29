c2a424a120f9fbf9dc3b8ea5fdc93f739dbc4f8e
LemonMan67 <liammcnally800@gmail.com>
2026-01-29 13:09:50 +0000

    buh

--- Process/endturn.py
+++ Process/endturn.py
@@ -8,3 +8,24 @@ def buh (counter):
     data["counter"] = counter
     Json.writejson (data, Path ("./json/save.json") , 2)
     return counter
+
+def rescource (iron, rawiron, coal, ironmine, ironfoundry, coalmine):
+    data = Json.loadjson (Path ("./json/save.json"))
+    info = Json.loadjson (Path ("./json/building.json"))
+    #load raw resources first to enable proper usage of processing buildings
+    rawiron = rawiron + (info ["type"] [1] ["details"] ["output"] ["raw iron"] * ironmine) #the 1 shows the index of the iron mine in "type"
+    coal = coal + (info ["type"] [0] ["details"] ["output"] ["coal"] * coalmine) 
+    
+    #calcualate all processed resources
+    iron = iron + (info ["type"] [2] ["details"] ["output"] ["iron"] * ironfoundry)
+    rawiron = rawiron - (info ["type"] [2] ["details"] ["output"] ["iron"] * ironfoundry)
+    if rawiron < 0: #if raw iron falls below 0, reverse what i just did 
+        iron = iron - (info ["type"] [2] ["details"] ["output"] ["iron"] * ironfoundry)
+        rawiron = rawiron + (info ["type"] [2] ["details"] ["output"] ["iron"] * ironfoundry)
+
+    #save all value to the save file
+    data ["iron"] = iron
+    data ["raw iron"] = rawiron
+    data ["coal"] = coal
+    Json.writejson (data, Path ("./json/save.json") , 2)
+
--- main.py
+++ main.py
@@ -39,27 +39,58 @@ class Battalion:
                 check = y["era"]["type"]
                 check = str(check)
                 listcheck = str(self.sortlist[self.passthrough])
-                if check == listcheck:    #make this check pass, correct value passed, but the value has extra characters making it not equal
+                if check == listcheck:  
                   print (f"  has { y["era"][stat]} {stat}   ( {y["era"]["type"]} )")
             stat = input ("\nwhich stat do you want to check? (hp, attack, org, defense, breakthrough, pierce, armour, AA, recon, entrenchment) or type stop to exit: ")
           else:
             print("invalid stat")
             stat = input ("\nwhich stat do you want to check? (hp, attack, org, defense, breakthrough, pierce, armour, AA, recon, entrenchment) or type stop to exit: ")
 
+
+class Building:
+   def __init__(self , filepath = "./json/building.json"):
+      self.path = Path (filepath)
+      self.data = Json.loadjson (self.path)
+      self.savepath = Path ("./json/save.json")
+      self.save = Json.loadjson (self.savepath)
+      self.money = load.money
+
+   def buybuilding (self , buildingname ) :
+      for x in self.data ["type"]:
+         if x ["details"]["building"] == buildingname :
+           if self.money >=  int ( x ["details"]["cost"]):
+              self.money -= int ( x ["details"]["cost"])
+              load.money = self.money
+              amount = self.save [buildingname]
+              amount += 1         #make way to print the correct value in the building menu
+              self.save [buildingname] = amount
+              self.save ["money"] = self.money
+              Json.writejson (self.save, self.savepath , 2)
+
+
+
+
+
+
+
 class Load:
    def __init__(self , filepath = "./json/save.json"):
       self.path = Path (filepath)
       self.data = Json.loadjson (self.path)
-
-   def saveload (self) :
-      global counter
-      counter =   self.data ["counter"]
+      self.counter = self.data ["counter"]
+      self.money = self.data ["money"]
+      self.raw_iron = self.data ["raw iron"]
+      self.iron = self.data ["iron"]         # make sure i figure out how to alter the building values with this new approah
+      self.coal = self.data ["coal"]
+      self.ironmine = self.data ["iron mine"]
+      self.ironfoundry = self.data ["iron foundry"]
+      self.coalmine = self.data ["coal mine"]
       
 
 load = Load()
 battalion = Battalion()
+building = Building()
 
-load.saveload()
 
 repeatmenu = 1
 menu = "0"
@@ -70,8 +101,9 @@ while repeatmenu == 1:
       os.system('cls' if os.name == 'nt' else 'clear')
       print("main menu:")
       print("\n1 : battalion interactions")
-      print("2 : end turn       current turn = ", counter) 
-      print("3 : exit")
+      print("2 : build/upgrade structures      current money = ", load.money)
+      print("3 : end turn       current turn = ", load.counter) 
+      print("4 : exit")
       menu = input ("\nselect menu: ")
       
    elif menu == "1":
@@ -90,15 +122,27 @@ while repeatmenu == 1:
          else:
             print("invalid menu")
             time.sleep(2)
-            
+
    elif menu == "2":
       os.system('cls' if os.name == 'nt' else 'clear')
+      print(load.ironmine, " iron mines owned")
+      print(load.ironfoundry, " iron foundries owned")
+      print(load.coalmine, " coal mines owned")
+      buy = input ("\nbuilding name: ")
+      building.buybuilding (buy)
+      menu = "0"
+
+      
+            
+   elif menu == "3":
+      os.system('cls' if os.name == 'nt' else 'clear')
       print("ending turn...")
-      counter = endturn.buh(counter)
+      load.counter = endturn.buh(load.counter)
+      endturn.rescource (load.iron, load.raw_iron, load.coal, load.ironmine, load.ironfoundry, load.coalmine)
       time.sleep(2)
       menu = "0"  
 
-   elif menu == "3":
+   elif menu == "4":
       print("exiting and saving...")
       # add save function here???
       time.sleep(3)
