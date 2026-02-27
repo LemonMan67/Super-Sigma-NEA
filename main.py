f1a174d5878bc2b835f0df2b235eb7424a8b8ae3
LemonMan67 <liammcnally800@gmail.com>
2026-02-27 23:11:12 +0000

    im gregging my tech

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class Battalion:
               print (f" Unit:  {x['unit']} ")
               for y in x ["list"]:
                 check = y["era"]["type"]
-                check = str(check)
+                check = str(check)                                 #this function sorts through the json file to return the required stat for each unit, all stats are the same for each batallion
                 listcheck = str(self.sortlist[self.passthrough])
                 if check == listcheck:  
                   print (f"  has { y["era"][stat]} {stat}   ( {y["era"]["type"]} )")
@@ -63,20 +63,39 @@ class Building:
                self.money -= int ( x ["details"]["cost"])
                load.money = self.money
                amount = self.save [buildingname]
-               amount += 1         #make way to print the correct value in the building menu
+               amount += 1        
                self.save [buildingname] = amount
                self.save ["money"] = self.money
                Json.writejson (self.save, self.savepath , 2)
                if buildingname == "iron mine":        
                      load.ironmine += 1
-               elif buildingname == "iron foundry":         #must add more lines here to actually increase the building count for a specific building
+               elif buildingname == "iron foundry":         
                       load.ironfoundry += 1
                elif buildingname == "coal mine":
                       load.coalmine += 1
       else:
         print("invalid building name")
         time.sleep(2)
-
+   
+   def sellrescource (self , rescourcename , amount ):
+      if rescourcename == "iron": #differnt prices for different rescources can be added here
+         sellprice = 2
+      elif rescourcename == "coal":
+         print("u cant sell this u simeon")
+         sellprice = 0
+      amount = int (amount)
+      while True:   
+        if amount <= self.save [rescourcename]: 
+           self.money += sellprice * int (amount)
+           self.save [rescourcename] -= int (amount)
+           self.save ["money"] = self.money
+           Json.writejson (self.save, self.savepath , 2)
+           load.money = self.money
+        else:
+             print("you dont have that much to sell")
+             time.sleep(2)
+        break
+      
                
 class Load:
    def __init__(self , filepath = "./json/save.json"):
@@ -106,8 +125,9 @@ while repeatmenu == 1:
       print("main menu:")
       print("\n1 : battalion interactions")
       print("2 : build/upgrade structures      current money = ", load.money)
-      print("3 : end turn       current turn = ", load.counter) 
-      print("4 : exit")
+      print("3 : rescource interactions")
+      print("4 : end turn                      current turn = ", load.counter) 
+      print("5 : exit")
       menu = input ("\nselect menu: ")
       
    elif menu == "1":
@@ -135,19 +155,55 @@ while repeatmenu == 1:
         print("2 : return")
         submenu = input ("\nselect menu: ")
         if submenu == "1":
-           os.system('cls' if os.name == 'nt' else 'clear')
-           build = Json.loadjson ("./json/building.json")
-           count = Json.loadjson ("./json/save.json")
-           print(f"coal mine:  {count["coal mine"]} owned , {build["type"][0]["output"]["coal"]} coal/sec, {build["type"][0]["cost"]} cost")    # here we need to add cost of building and amount owned and production rate
-           print(f"iron mine: {count["iron mine"]} owned , {build["type"][1]["output"]["raw iron"]} raw iron/sec, {build["type"][1]["cost"]} cost")
-           print(f"iron foundry: {count["iron foundry"]} owned , {build["type"][2]["output"]["iron"]} iron/sec, this uses {build["type"][2][input]["coal"]} coal and {build["type"][2][input]["raw iron"]} raw iron/sec {build["type"][2]["cost"]} cost")
-           buy = input ("\npurchase : ")        #completely remake this menu
-           building.buybuilding (buy)
-           menu = "0"
-
-      
-            
+           while True:
+             os.system('cls' if os.name == 'nt' else 'clear')
+             build = Json.loadjson ("./json/building.json")
+             count = Json.loadjson ("./json/save.json")
+             print(f"current money: {load.money}\n")
+             print(f"coal mine:  {count["coal mine"]} owned , {build["type"][0]["details"]["output"]["coal"]} coal/sec,   {build["type"][0]["details"]["cost"]} cost")    # here we need to add cost of building and amount owned and production rate
+             print(f"iron mine: {count["iron mine"]} owned , {build["type"][1]["details"]["output"]["raw iron"]} raw iron/sec,   {build["type"][1]["details"]["cost"]} cost")
+             print(f"iron foundry: {count["iron foundry"]} owned , {build["type"][2]["details"]["output"]["iron"]} iron/sec, uses {build["type"][2]["details"]["input"]["coal"]} coal and {build["type"][2]["details"]["input"]["raw iron"]} raw iron/sec,   {build["type"][2]["details"]["cost"]} cost")
+             buy = input ("\npurchase ('exit' to leave): ")      
+             if buy == "exit":
+                break
+             elif buy == "coal mine" or buy == "iron mine" or buy == "iron foundry":
+                building.buybuilding(buy)
+             else:
+                print("invalid building")
+                time.sleep(2)
+        elif submenu == "2":
+            menu = "0"
+            break
+        
    elif menu == "3":
+        while True:
+          os.system('cls' if os.name == 'nt' else 'clear')
+          print("rescource interactions:")
+          print("\n1 : sell rescources (coal and ore cant be sold)")
+          print("2 : return")
+          submenu = input ("\nselect menu: ")
+          if submenu == "1":
+            while True:
+               os.system('cls' if os.name == 'nt' else 'clear')
+               count = Json.loadjson ("./json/save.json")
+               print(f"current money: {load.money}\n")
+               print(f"iron: {count['iron']} owned")
+               sell = input ("\nsell ('exit' to leave): ")
+               if sell == "exit":
+                  break
+               elif sell == "iron": #or sell == blah blah blah
+                  amount = input ("amount to sell: ")
+                  building.sellrescource(sell, amount)
+               else:
+                  print("invalid rescource")
+                  time.sleep(2)
+          elif submenu == "2":
+              menu = "0"
+              break
+
+
+            
+   elif menu == "4":
       os.system('cls' if os.name == 'nt' else 'clear')
       print(load.iron, load.rawiron, load.coal)
       print("ending turn...")
@@ -157,7 +213,7 @@ while repeatmenu == 1:
       time.sleep(5)
       menu = "0"  
 
-   elif menu == "4":
+   elif menu == "5":
       print("exiting and saving...")
       # add save function here???
       time.sleep(3)
